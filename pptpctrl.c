waynedkim/poptop

commit a8892d29720135b9224de28b767e9fb17482f5fb

From: Christoph Biedl <sourceforge.bnwi@manchmal.in-ulm.de>
Description: "remotenumber" support
Author: Igor A Tarasov <develop@dicr.org>
Author: Michael Poetters <michael@poetters.net>
Bug-Debian: http://bugs.debian.org/610375
Bug-Debian: http://bugs.debian.org/625459
Bug-Ubuntu: https://bugs.launchpad.net/ubuntu/+source/pptpd/+bug/704245

pptpctrl.c
@@ -3,7 +3,7 @@
  *
  * PPTP control connection between PAC-PNS pair
  *
- * $Id: pptpctrl.c,v 1.23 2011/05/19 00:02:50 quozl Exp $
+ * $Id: pptpctrl.c,v 1.24 2013/02/07 00:25:44 quozl Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -673,7 +673,7 @@ static int startCall(char **pppaddrs, struct in_addr *inetaddrs)
  */
 static void launch_pppd(char **pppaddrs, struct in_addr *inetaddrs)
 {
-	char *pppd_argv[14];
+	char *pppd_argv[16];
 	int an = 0;
 	sigset_t sigs;
 
@@ -793,6 +793,9 @@ static void launch_pppd(char **pppaddrs, struct in_addr *inetaddrs)
                  pppd_argv[an++] = inet_ntoa(inetaddrs[1]);
         }
 #endif
+
+	pppd_argv[an++] = "remotenumber";
+	pppd_argv[an++] = inet_ntoa(inetaddrs[1]);
 
 	/* argv arrays must always be NULL terminated */
 	pppd_argv[an++] = NULL;

commit 7ac03f8e907dc8ca1077c7fc1c9ceb61dbd2da92

avoid remotenumber option on FreeBSD
Regression on *BSD only, introduced by
a8892d29720135b9224de28b767e9fb17482f5fb
"From: Christoph Biedl <sourceforge.bnwi@manchmal.in-ulm.de>"

Signed-off-by: James Cameron <quozl@laptop.org>
Reported-by: Aprigio De Faria <screenblack@gmail.com>

pptpctrl.c
@@ -790,10 +790,10 @@ static void launch_pppd(char **pppaddrs, struct in_addr *inetaddrs)
                  pppd_argv[an++] = "pptpd-original-ip";
                  pppd_argv[an++] = inet_ntoa(inetaddrs[1]);
         }
-#endif
 
         pppd_argv[an++] = "remotenumber";
         pppd_argv[an++] = inet_ntoa(inetaddrs[1]);
+#endif
 
         /* argv arrays must always be NULL terminated */
         pppd_argv[an++] = NULL;